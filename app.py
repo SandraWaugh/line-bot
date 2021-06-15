f7235fb9e1a1497c01b7d1bc615c8e6ed53e82e3
Sandra <kshysei@gmail.com>
2021-06-15 18:33:33 +0900

    update

--- app.py
+++ app.py
@@ -38,9 +38,11 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    msg:event.message.text
+    s = "How are you"
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=s))
 
 
 if __name__ == "__main__":    #執行main function的時候，來確定該檔案是直接被執行，而不是被載入而已
